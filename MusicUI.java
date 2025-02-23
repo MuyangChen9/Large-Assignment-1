8d4c8a38e3605be6049cb6f2682e1e90ad469881
BryanFrank82 <148111531+BryanFrank82@users.noreply.github.com>
2025-02-23 14:03:32 -0700

    MusicUI.java added
    Added MusicUI.java

---
+++ MusicUI.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class MusicUI extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage stage) {
+		stage.setTitle("WildCat Music");
+	}
+}
