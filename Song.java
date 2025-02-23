fcb3c220d79e8761f4a0f7c693203cf7ff5650a2
MuyangChen9 <muyangchen@arizona.edu>
2025-02-23 15:23:45 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -9,15 +9,6 @@ public class Song {
     private int year;
     private String content;
 
-    public Song(String title, String artist, String album, String genre, int year) {
-        this.title = title;
-        this.artist = artist;
-        this.album = album;
-        this.genre = genre;
-        this.year = year;
-        content = "";
-    }
-
     public String getTitle() {
         return title;
     }
@@ -44,15 +35,20 @@ public class Song {
     public Song(String songName) {
         Scanner scanner = new Scanner(songName);
         String [] getName = songName.split("_");
-        String songTitle = getName[0];
+        String title = getName[0];
 
         String firstLine = scanner.nextLine();
         String[] data = firstLine.split(",");
-        String getAlbum = data[0].trim();
-        String getArtist = data[1].trim();
-        String getGenre = data[2].trim();
-        int getYear = Integer.parseInt(data[3].trim());
-        new Song(songTitle, getArtist, getAlbum, getGenre, getYear);
+        String album = data[0].trim();
+        String artist = data[1].trim();
+        String genre = data[2].trim();
+        int year = Integer.parseInt(data[3].trim());
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.year = year;
+        content = "";
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine().trim();
             content+=line;
