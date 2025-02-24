d9a65d252d6a17cd8c7215be09b4790f4110bd4a
MuyangChen9 <muyangchen@arizona.edu>
2025-02-23 18:51:09 -0700

    Update Albums.java

--- Albums.java
+++ Albums.java
@@ -5,24 +5,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Albums {
-    private HashMap<String, String> songList;
+    private ArrayList<Song> songList;
 
-    public HashMap<String, String> getSongList() {
+    public ArrayList<Song> getSongList() {
     	return songList;
     }
     
     public Albums(String albums)  throws FileNotFoundException{
+    	ArrayList<Song> songList = new ArrayList<>();
     	File file = new File(albums);
     	Scanner scanner = new Scanner(file);
-    	HashMap<String, String> songList = new HashMap<>();
-        while (scanner.hasNextLine()) {
-        String line = scanner.nextLine().trim();
-        String [] content = line.split(",");
-        String songName = content[0];
-        String singerName = content[1];
-        
-        
-        songList.put(songName, singerName);
+    	
+    	
+    	String firstLine = scanner.nextLine();
+        String[] data = firstLine.split(",");
+        String album = data[0].trim();
+        String artist = data[1].trim();
+        String genre = data[2].trim();
+        int year = Integer.parseInt(data[3].trim());
+        while(scanner.hasNextLine()) {
+        	String title = scanner.nextLine().trim();
+        	Song newSong = new Song(title,artist,album,year,genre);
+        	songList.add(newSong);
         }
         this.songList = songList;
             
