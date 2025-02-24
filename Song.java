0d47cf6b0d025d4148ad4276c6488f283c09682d
MuyangChen9 <muyangchen@arizona.edu>
2025-02-23 17:16:46 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Song {
+	
     private String title;
     private String artist;
     private String album;
     private String genre;
     private int year;
     private String content;
-}
+
+	
 
     public String getTitle() {
         return title;
@@ -18,22 +20,26 @@ public class Song {
         return artist;
     }
 
+    
     public String getAlbum() {
         return album;
     }
+    
     
     public String getGenre() {
     	return genre;
     }
     
+    
     public int getYear() {
     	return year;
     }
     
+    
     public String getcontent() {
     	return content;
     }
-    //read the file,get the information and the content for the song
+    
     public Song(String songName) {
         Scanner scanner = new Scanner(songName);
         String [] getName = songName.split("_");
@@ -56,7 +62,6 @@ public class Song {
             content+=line;
             content+="\n";
         }
-
         scanner.close();
     }
-
+}
