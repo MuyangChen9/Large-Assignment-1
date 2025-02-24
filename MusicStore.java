e53b98f07915580370807d21603b20c46d278660
MuyangChen9 <muyangchen@arizona.edu>
2025-02-23 18:58:09 -0700

    Update MusicStore.java

--- MusicStore.java
+++ MusicStore.java
@@ -1,18 +1,17 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MusicStore {
     private ArrayList<Albums> album;
     private ArrayList<Song> songList;
-
+    
     public MusicStore() {
     	ArrayList<Albums> album;
     	ArrayList<Song> songList;
     }
     
     
-    
-//getter
     public ArrayList<Albums> getAlbums() {
         return album;
     }
@@ -20,7 +19,7 @@ public class MusicStore {
     public ArrayList<Song> getSongList(){
     	return songList;
     }
-    //search song by title
+    
     public ArrayList<Song> searchSongByTitle(String title) {
         ArrayList<Song> result = new ArrayList<>();
         for (Song s : songList) {
@@ -30,7 +29,7 @@ public class MusicStore {
         }
         return result;
     }
-//search song by artist
+
     public ArrayList<Song> searchSongByArtist(String artist) {
         ArrayList<Song> result = new ArrayList<>();
         for (Song s : songList) {
@@ -40,30 +39,29 @@ public class MusicStore {
         }
         return result;
     }
-//search album by title
+
     public ArrayList<Albums> searchAlbumByTitle(String title) {
         ArrayList<Albums> result = new ArrayList<>();
         for (Albums a : album) {
-            if (a.getSongList().containsKey(title)) {
+            if (a.getAlbumsName().equals(title)) {
                 result.add(a);
             }
         }
         return result;
     }
-//search album by artist
+
     public ArrayList<Albums> searchAlbumByArtist(String artist) {
         ArrayList<Albums> result = new ArrayList<>();
         for (Albums a : album) {
-            if (a.getSongList().equals(artist)) {
+            if (a.getArtist().equals(artist)) {
                 result.add(a);
             }
         }
         return result;
     }
-    //read file, andr put the data into list
-    public void addSong(String songName) throws FileNotFoundException{
-    	Song newSong = new Song(songName);
-    	songList.add(newSong);
+    
+    public void addSong(Song songName){
+    	songList.add(songName);
     }
     
     public void addAlbums(String albumsName) throws FileNotFoundException{
