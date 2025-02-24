34ffafcd9fb53fd66adce2b99e8c9be11f5128b5
MuyangChen9 <muyangchen@arizona.edu>
2025-02-24 16:11:32 -0700

    Update Rate.java

--- Rate.java
+++ Rate.java
@@ -8,7 +8,7 @@ public class Rate {
     public int getRate() {
         return rate;
     }
-
+//set the rate and make sure the rating is between 0 and 5.
     public void setRate(int rate) {
         if (rate > 0 && rate < 6) {
             this.rate = rate;
